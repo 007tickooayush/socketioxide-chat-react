b76203047e795f1ac0e8c88400bbc9d6385e2606
007tickooayush <ayush.tickoo292000@gmail.com>
2024-06-06 10:31:52 +0530

    MUI Core setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import { Button, Container, Typography } from '@mui/material'
+
+// import './App.css'
 
 function App() {
 	const [count, setCount] = useState(0)
-  
+
 	return (
-		<>
+		<Container maxWidth='xl' sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', padding: 4 }}>
 			<div>
 				<a href="https://vitejs.dev" target="_blank">
 					<img src={viteLogo} className="logo" alt="Vite logo" />
@@ -17,18 +19,18 @@ function App() {
 				</a>
 			</div>
 			<h1>Vite + React</h1>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>
+			<Container sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', padding: 4 }}>
+				<Button variant='outlined' onClick={() => setCount((count) => count + 1)}>
 					count is {count}
-				</button>
-				<p>
-          			Edit <code>src/App.jsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">
-        		Click on the Vite and React logos to learn more
-			</p>
-		</>
+				</Button>
+				<Typography variant='body1'>
+					Edit <code>src/App.jsx</code> and save to test HMR
+				</Typography>
+			</Container>
+			<Typography variant='body2'>
+				Click on the Vite and React logos to learn more
+			</Typography>
+		</Container>
 	)
 }
 
