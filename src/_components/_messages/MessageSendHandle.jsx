ddd2720638bdfccd1b1a4d90f40307ef46216f6d
007tickooayush <ayush.tickoo292000@gmail.com>
2024-06-14 18:54:52 +0530

    general chat implementation + pending display and socket state handling

--- src/_components/_messages/MessageSendHandle.jsx
+++ src/_components/_messages/MessageSendHandle.jsx
@@ -1,15 +1,41 @@
 import { Box, Button, FormControl, Grid, TextField } from '@mui/material'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { socket } from '../../_utils/socket';
 
 const MessageSendHandle = () => {
+
+    const [sentMsg, setSentMsg] = useState('');
+
+    useEffect(() => {
+        socket.on('response', (data) => {
+            console.log('data :>> ', data);
+        });
+
+        return () => {
+            socket.off('response');
+        }
+    }, [socket, sentMsg]);
+
+    const handleSendMessage = (msg) => {
+        setSentMsg(msg);
+        // console.log('sentMsg :>> ', sentMsg);
+    };
+
+    const handleSendSocket = () => {
+        if (sentMsg) {
+            socket.emit("message", { room: "general", message: sentMsg });
+            setSentMsg('');
+        }
+    }
+
     return (
         <FormControl sx={{ width: '100%' }}>
             <Grid container sx={{ placeItems: 'center' }}>
-                <Grid item xl={11} lg={11} md={11} sm={10} xs={10}>
-                    <TextField id='sentMsg' label='Message' fullWidth />
+                <Grid item xl={11} lg={11} md={11} sm={11} xs={11} paddingRight={2}>
+                    <TextField id='sentMsg' label='Message' value={sentMsg} onChange={(e) => handleSendMessage(e.target.value)} fullWidth />
                 </Grid>
-                <Grid item xl={1} lg={1} md={1} sm={2} xs={2} height={'100%'}>
-                    <Button variant='outlined' fullWidth sx={{ height: '100%', padding: 2 }}>Send</Button>
+                <Grid item xl={1} lg={1} md={1} sm={1} xs={1} height={'100%'}>
+                    <Button variant='outlined' fullWidth sx={{ height: '100%', padding: 2 }} onClick={handleSendSocket}>Send</Button>
                 </Grid>
             </Grid>
 
